0a9be10c3c4e6ec90339ac024e74611baf146fe8
EdJeeOnGitHub <edjee96@gmail.com>
2021-04-21 16:46:23 -0500

    fixed names

--- code/replicate-main-findings.R
+++ code/replicate-main-findings.R
@@ -86,38 +86,29 @@ create_ols_formula <- function(base_formula = base_ols_formula,
 }
 
 # Create formulae
-ols_formulae_2_4 <- list(
-    reservation_controls,
+ols_formulae <- list(
+    "1", # base formula + intercept
+    reservation_controls, 
     c(reservation_controls, tribe_controls),
-    c(reservation_controls, tribe_controls, additional_reserve_controls)
+    c(reservation_controls, tribe_controls, additional_reserve_controls),
+    c(reservation_controls, tribe_controls, additional_reserve_controls, "0 | statenumber") # + state FE
 ) %>%
     map(~create_ols_formula(base_ols_formula,
                             .x))
 
 # Fit OLS models
-ols_models_1 <- feols(
-    fml = as.formula(base_ols_formula),
-    data = native_current_df,
-    cluster = ~eatribe + statenumber
-)
-
-ols_models_2_4 <- map(ols_formulae_2_4,
+ols_models <- map(ols_formulae,
     ~feols(
         fml = .x,
         data = native_current_df,
-        cluster = ~ eatribe + statenumber
+        cluster = ~ eaid + statenumber
     ))
 
-ols_models_5 <- feols(
-    fml = as.formula(paste0(paste0(deparse(ols_formulae_2_4[[3]]), collapse = ""), "| statenumber", collapse = "")),
-    data = native_current_df,
-    cluster = ~eatribe + statenumber
-)
 
 # Checking we have replicated the tables correctly
 test_that("Table 2 OLS replications", {
     # read directly off table 2A
-    dippel_FC_coefs <- c(
+    dippel_FC_ols_coefs <- c(
         -0.358,
         -0.334,
         -0.364,
@@ -127,10 +118,8 @@ test_that("Table 2 OLS replications", {
     # Estimated using our models
     # following pipe just extracts
     # coefs
-    rep_FC_coefs <- c(
-        list(ols_models_1),
-        ols_models_2_4,
-        list(ols_models_5)
+    rep_FC_ols_coefs <- c(
+        ols_models
     ) %>%
     map(tidy) %>%
     map(filter, term == "FC") %>%
@@ -139,8 +128,89 @@ test_that("Table 2 OLS replications", {
     unlist()
     # compare each element
     map2(
-        rep_FC_coefs,
-        dippel_FC_coefs,
+        rep_FC_ols_coefs,
+        dippel_FC_ols_coefs,
+        expect_equal,
+        tolerance = 0.001
+    )
+
+})
+
+# creating dictionaries to clean up terms:
+group_replacement <- list(
+        "Reservation Controls" = reservation_controls,
+        "Tribe Controls" = tribe_controls,
+        "Extra Reservation Controls" = additional_reserve_controls
+)
+dict_replacment <- c(
+        eaid = "Tribe",
+        statenumber = "State",
+        logpcinc = "log(per capita income)",
+        FC = "Forced coexistence",
+        HC = "Historical centralisation"
+)
+# aggregating models and saving to tex file
+etable(
+    ols_models,
+    title = "OLS - Dippel Table 2, Panel A",
+    drop = c("(Intercept)"),
+    group = group_replacement,
+    dict = dict_replacment,
+    tex = TRUE,
+    replace = TRUE,
+    file = "data/output/table-2-ols.tex",
+    digits = 3,
+    coefstat = "tstat"
+)
+
+# Now panel B
+# Create FE formulae
+# We:
+#   - Add "|eatribe" FE to the OLS formula,
+#   - convert formulae to strings so we can remove brackets
+#       and fix the case that already has state FE as the  syntax
+#       is scuffed otherwise.
+#   - convert back into formulae objects
+fe_formulae <- ols_formulae %>%
+    map(~update(.x, ~ . | eaid)) %>%
+    map(deparse) %>%
+    map(~str_remove_all(.x, "\\(|\\)") %>%paste(collapse = "")) %>%
+    map(~str_replace_all(.x, "(?<=statenumber )\\|(?= eaid)", "+")) %>%
+    map(as.formula)
+# Fit FE models
+fe_models <- map(fe_formulae,
+    ~feols(
+        fml = .x,
+        data = native_current_df,
+        cluster = ~eaid + statenumber
+    ))
+
+
+
+# Testing replication
+test_that("Table 2 Panel B Replicates", {
+    dippel_FC_fe_coefs <- c(
+        -0.401,
+        -0.318,
+        -0.318,
+        -0.274,
+        -0.276
+    )
+    # Estimated using our models
+    # following pipe just extracts
+    # coefs
+    rep_FC_fe_coefs <- c(
+        fe_models
+    ) %>%
+    map(tidy) %>%
+    map(filter, term == "FC") %>%
+    map(select, estimate) %>%
+    map(pull) %>%
+    unlist()
+    # compare each element
+    map2(
+        rep_FC_fe_coefs,
+        dippel_FC_fe_coefs,
         expect_equal,
         tolerance = 0.001
     )
@@ -148,29 +218,18 @@ test_that("Table 2 OLS replications", {
 })
 
 
-c(
-        list(ols_models_1),
-        ols_models_2_4,
-        list(ols_models_5)
-    ) 
-    
-# aggregating models and saving to tex file
+
 etable(
-    ols_models_1,
-    ols_models_2_4,
-    ols_models_5,
-    title = "OLS - Dippel Table 2, Panel A",
-    drop = c("(Intercept)"),
-    group = list(
-        "Reservation Controls" = reservation_controls,
-        "Tribe Controls" = tribe_controls,
-        "Extra Reservation Controls" = additional_reserve_controls
-    ),
-    dict = c(
-        eatribe = "Tribe",
-        statenumber = "State"
-    ),
+    fe_models,
+    title = "FE - Dippel Table 2, Panel B",
+    group = group_replacement,
+    dict = dict_replacment,
+    coefstat = "tstat",
     tex = TRUE,
     replace = TRUE,
-    file = "data/output/table-2-ols.tex"
+    file = "data/output/table-2-fe.tex",
+    digits = 3
 )
+
+
+
